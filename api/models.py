2f0fbd994664b5cd3f1899ca68ffb7852074a59a
ZARIFMAC <uzbzarif@gmail.com>
2023-03-16 10:58:35 +0500

    add model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task = models.CharField(max_length=20)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task
+    
+# Kill the port 8000
+# sudo kill -9 $(sudo lsof -t -i:8000)
